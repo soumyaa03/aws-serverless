7b0585353a6afe285bc8cb476c1d43a864cc23cf
soumyaa03 <82571869+soumyaa03@users.noreply.github.com>
2023-02-14 22:21:10 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/soumyaa3/aws-serverless
+
+go 1.19
+
+require github.com/aws/aws-sdk-go v1.44.199
+
+require (
+	github.com/aws/aws-lambda-go v1.37.0
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
---
+++ pkg/validators/is_email_valid.go
@@ -0,0 +1,19 @@
+package validators
+
+import (
+	"net/mail"
+	// "regexp"
+)
+
+func IsEmailValid(email string) bool {
+
+	// var rxEmail = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]{1,64}@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	// if len(email) < 3 || len(email) > 254 || !rxEmail.MatchString(email){
+	// 	return false
+	// }
+
+	// return true
+
+	_, err := mail.ParseAddress(email)
+	return err == nil
+}
